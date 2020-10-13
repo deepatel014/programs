afdcf9ff28647ae930e1718a98bfe5de505d557a
Deep <deepp3021@gmail.com>
2020-10-13 20:13:47 +0530

    Thiss code helps in clearing the concept of merging

--- mergeTools.py
+++ mergeTools.py
@@ -1,1 +1,15 @@
+def merge_tool(string,z):
+    while string:
+        seen = string[0:z]
+        s = ''
+        for c in seen:
+            if c not in s:
+                s += c
+        print(s)
+        string = string[z:]
 
+
+
+s = input()
+i = int(input())
+merge_tool(s,i)
