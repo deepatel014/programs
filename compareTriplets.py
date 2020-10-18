bf3d8d89bd11a94aeafbe975d9463917286c7a76
Deep Patel <deepp3021@gmail.com>
2020-10-18 23:34:53 +0530

    Update compareTriplets.py
    code compares the values between two lists.

--- compareTriplets.py
+++ compareTriplets.py
@@ -1,1 +1,35 @@
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    acount = 0
+    bcount = 0
+    for i in range(0,len(a)):
+        if a[i]>b[i]:
+            acount +=1
+        elif a[i]<b[i]:
+            bcount +=1
+        else:
+            acount += 0
+            bcount += 0
+    return "{}{}".format(acount,bcount)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
